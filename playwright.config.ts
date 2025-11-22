c6c9663d0975586f6b5f4ae60a96600b575937f5
Jamal Bankole <jbankole1212@gmail.com>
2025-11-21 19:30:22 -0500

    Edited webserver command

--- playwright.config.ts
+++ playwright.config.ts
@@ -85,7 +85,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'cd client; npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
