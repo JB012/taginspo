3ea05c9146340b5d8d47494f29196b2de3ae7796
Jamal Bankole <jbankole1212@gmail.com>
2025-12-08 21:07:18 -0500

    Implemented axios calls to API

--- client/src/routes/EditImage.tsx
+++ client/src/routes/EditImage.tsx
@@ -1,17 +1,25 @@
 import { FaArrowLeft, FaCheck, FaPlus } from "react-icons/fa";
 import { FaCircleXmark, FaX } from "react-icons/fa6";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router";
+import TagType from '../../types/TagType';
+import axios from "axios";
 
 export default function EditImage() {
-    const [tagInput, setTagInput] = useState(false);
+    const [title, setTitle] = useState('');
+    const [source, setSource] = useState('');
+    const [tagInput, setTagInput] = useState('');
+    const [addedTags, setAddedTags] = useState<Array<TagType>>([]);
+    const [addTag, setAddTag] = useState(false);
+    const [allTags, setAllTags] = useState<Array<TagType> | null>(null);
+    const [tagSearchResults, setTagSearchResults] = useState<Array<TagType>>([]);
     const navigate = useNavigate();
+    const imageRef = useRef<HTMLImageElement | null>(null);
 
     useEffect(() => {
         const dropZone = document.querySelector('#drop-zone') as HTMLElement;
         const dropZoneContainer = document.querySelector('#drop-zone-container') as HTMLElement;
         const fileInput = document.querySelector('#add-image') as HTMLInputElement;
-        const preview = document.querySelector("#preview-image");
         const message = document.querySelector("#drop-zone-message") as HTMLElement;
         const clearBtn = document.querySelector("#clear-image") as HTMLElement;
 
@@ -78,15 +86,13 @@ export default function EditImage() {
         }
 
         function handleClearButton() {
-             if (preview) {
-                const img = preview.querySelector('img');
+            if (imageRef.current) {
+                URL.revokeObjectURL(imageRef.current.src);
 
-                if (img) {
-                    URL.revokeObjectURL(img.src);
-                    preview.removeChild(img);
-                }
+                imageRef.current.removeAttribute('src');
+                imageRef.current.removeAttribute('alt');
+                imageRef.current.style.display = "none";
 
-                
                 if (message) {
                     clearBtn.style.display = "none";
                     message.style.display = "block";
@@ -97,31 +103,24 @@ export default function EditImage() {
                     // Disabling file input since an image is currently displayed.
                     fileInput.disabled = false;
                 }
-                
             }
         }
 
         function displayImage(file : File | null) {
             if (file?.type.startsWith("image/")) {
-                const img = document.createElement("img");
                 
-                img.src = URL.createObjectURL(file);
-                img.alt = file.name;
-
-                img.style.width = "100%";
-                img.style.height = "100%";
-                img.style.objectFit = "fill";
-                
-                
-                if (preview) {
-                    preview.appendChild(img);
-
+                if (imageRef.current) {
+                    imageRef.current.src = URL.createObjectURL(file);
+                    imageRef.current.alt = file.name;
+                    imageRef.current.style.display = "block";
+                    
                     message.style.display = "none";
                     clearBtn.style.display = "block";
                     dropZoneContainer.style.outlineStyle = "none";
                     dropZone.style.cursor = "default";
                     fileInput.disabled = true;
                 }
+                
             }
         }
 
@@ -131,7 +130,6 @@ export default function EditImage() {
                 const file = ev.dataTransfer!.items[0].getAsFile();
                 displayImage(file);
             }
-            
         }
 
         
@@ -142,6 +140,12 @@ export default function EditImage() {
         fileInput?.addEventListener('input', handleFileInput);
         dropZone?.addEventListener("drop", handleDrop);
 
+        if (!allTags) {
+            axios.get('http://localhost:3000/tags').then(res => {
+                setAllTags(res.data);
+            });
+        }
+
         return () => {   
             window.addEventListener('drop', handleWindowDrop);
             window.addEventListener('dragover', handleDragOver); 
@@ -150,7 +154,51 @@ export default function EditImage() {
             fileInput?.removeEventListener('input', handleFileInput);
             dropZone?.removeEventListener("drop", handleDrop);
         }
-    }, []);
+    }, [allTags]);
+
+    function handleSubmit() {
+        if (imageRef.current?.src) {
+            const bodyData = {
+                title: title !== "" ? title : new Date(),
+                source: source !== "" ? source : null,
+                tags: addedTags
+            }
+
+            axios.post('http://localhost:3000/images/add', bodyData).then(res => {
+                if (res.status === 200) {
+                    navigate('/gallery');
+                }
+            })
+        }
+    }
+
+    function handleAddTag() {
+        setAddedTags([...addedTags, {title: tagInput, color: "#JFWKKF"}]);
+        setTagInput("");
+        setTagSearchResults([]);
+    }
+
+
+    function handleTagSearch(event : React.ChangeEvent<HTMLInputElement>) {
+        setTagInput(event.target.value);
+        
+        const matchedTags = allTags?.filter(tag => tag.title.startsWith(tagInput));
+
+        if (matchedTags && matchedTags.length > 0) {
+            setTagSearchResults(matchedTags);
+        }
+        else {
+            setTagSearchResults([]);
+        }
+
+    }
+
+    function handleKeyDown(event : React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === "Enter" && tagInput !== "") {
+            handleAddTag();
+        }
+    }
+
     return (
         <div className="flex flex-col p-4 gap-10 w-full h-full">
             <div className="flex w-full justify-between">
@@ -166,29 +214,30 @@ export default function EditImage() {
                         </label>
                         <div id="preview-image" className="relative">  
                             <FaCircleXmark color="white" id="clear-image" className="hidden z-20 absolute top-3 right-3 cursor-pointer" size={20} scale={1}/>
+                            <img ref={imageRef} src={undefined} className="w-full h-full object-fill" />
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col gap-16">
                     <div className="flex flex-col gap-4">
                         <label htmlFor="title">Title</label>
-                        <input id="title" className="w-[464px] h-[50px] rounded-full outline outline-black" />
+                        <input value={title} onChange={(e) => setTitle(e.target.value)} id="title" className="w-[464px] h-[50px] rounded-full outline outline-black" />
                     </div>
                     <div className="flex flex-col gap-4">
                         <label htmlFor="title">Source</label>
-                        <input id="title" className="w-[464px] h-[50px] rounded-full outline outline-black" />
+                        <input value={source} onChange={(e) => setSource(e.target.value)} id="title" className="w-[464px] h-[50px] rounded-full outline outline-black" />
                     </div>
                     <div className="flex flex-col">
                         <div className="flex gap-3">
                             <div>Tags</div>
                             <div className="flex flex-col relative">
-                                <FaPlus onClick={() => setTagInput(true)} className={!tagInput ? "cursor-pointer" : "hidden"} size={20} scale={1}/>
-                                <div id="tag-input-container" className={tagInput ? "flex flex-col" : "hidden"}>
+                                <FaPlus onClick={() => setAddTag(true)} className={!addTag ? "cursor-pointer" : "hidden"} size={20} scale={1}/>
+                                <div id="tag-input-container" className={addTag ? "flex flex-col" : "hidden"}>
                                     <div className="flex gap-6">
-                                        <input placeholder="Enter tag here" multiple={false} className="w-[243px] h-[30px] rounded-full px-3 outline outline-black" />
+                                        <input value={tagInput} onChange={handleTagSearch} onKeyDown={handleKeyDown} placeholder="Enter tag here" multiple={false} className="w-[243px] h-[30px] rounded-full px-3 outline outline-black" />
                                         <div className="flex justify-between w-[60px]">
-                                            <FaCheck data-testid="submit-tag" onClick={() => setTagInput(false)} className="cursor-pointer" size={20} scale={1} />
-                                            <FaX data-testid="cancel-tag" onClick={() => setTagInput(false)} className="cursor-pointer" size={20} scale={1} />
+                                            <FaCheck data-testid="submit-tag" onClick={() => {setAddTag(false); handleAddTag()}} className="cursor-pointer" size={20} scale={1} />
+                                            <FaX data-testid="cancel-tag" onClick={() => {setAddTag(false); setTagInput(""); setTagSearchResults([])}} className="cursor-pointer" size={20} scale={1} />
                                         </div>
                                     </div>
                                     <div className="flex flex-col outline outline-black w-[239px] h-[200px] p-4 gap-4 overflow-y-auto">
